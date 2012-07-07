48b2bb259c697404d3b2cc951d60777242658ae4
infidel <aval57@yahoo.com>
2012-07-07 15:44:49 -0400

    updated jumpstart.bat

--- util/jumpstart.bat
+++ util/jumpstart.bat
@@ -1,11 +1,14 @@
 @echo off
 
-:: edit the 2 paths below if necessary
+:: edit the 3 paths below if necessary
+::    - you may want to add other interpreter paths if they're not installed system-wide, e.g.
+::         set OTHERS=c:\Perl\bin;c:\ruby\bin
 set PYTHON=c:\Python27
+set OTHERS=
 set JAVA_HOME="c:\Program Files (x86)\Java\jdk1.6.0_12"
 
 set lib=%~dp0\lib
-if not "%path_set%"=="yes" set path=%PYTHON%;%PATH%
+if not "%path_set%"=="yes" set path=%PYTHON%;%OTHERS%;%PATH%
 set path_set=yes
 
 %JAVA_HOME%\bin\java.exe -cp "%lib%/jumpstart.jar;%lib%/py4j0.7.jar;%lib%/commons-exec-1.1.jar" jumpstart %*
