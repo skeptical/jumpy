74646a10348a2c89f9e30f07a69d302995badb2c
infidel <aval57@yahoo.com>
2013-02-01 00:31:52 -0500

    doc, fmt, bump version

--- src/jumpy.java
+++ src/jumpy.java
@@ -53,7 +53,7 @@ import net.pms.external.DebugPacker;
 public class jumpy implements AdditionalFoldersAtRoot, dbgpack, DebugPacker {
 
 	public static final String appName = "jumpy";
-	public static final String version = "0.2.5";
+	public static final String version = "0.2.6";
 	private static final String msgtag = appName + ": ";
 	private PMS pms;
 	public PmsConfiguration configuration;
